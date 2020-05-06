651758aa986eec28f6b26d3ad1a061cbb343a2df
Kyle Denney <kyle_denney@yahoo.com>
2020-05-06 17:12:26 -0400

    Add project files.

---
+++ BaseDapperDatabaseHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDatabaseHelper
+{
+	public abstract class BaseRepository
+	{
+		private readonly string _connectionString;
+
+		protected BaseRepository(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		protected async Task<T> WithConnection<T>(Func<IDbConnection, Task<T>> executeAsync)
+		{
+			try
+			{
+				using (var connection = new SqlConnection(_connectionString))
+				{
+					await connection.OpenAsync();
+					return await executeAsync(connection);
+				}
+			}
+			catch (TimeoutException ex)
+			{
+				throw new Exception(String.Format("{0}.WithConnection() experienced a SQL timeout", GetType().FullName), ex);
+			}
+			catch (SqlException ex)
+			{
+				throw new Exception(String.Format("{0}.WithConnection() experienced a SQL exception", GetType().FullName), ex);
+			}
+		}
+	}
+}
---
+++ DatabaseHelper.cs
@@ -0,0 +1,113 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperDatabaseHelper
+{
+    public class DatabaseHelper<T> : BaseRepository
+    {
+        SqlConnection conn;
+
+        public DatabaseHelper(string connectionString) : base(connectionString) { }
+
+        public void ExecuteNonQuery(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        public SqlDataReader ExecuteReader(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+
+        public async Task<int> ExecuteCommandQueryAsync(string sql, params SqlParameter[] parameters)
+        {
+            return await WithConnection<int>(async c => {
+
+                List<T> list = new List<T>();
+
+                DynamicParameters dynamicParamters = new DynamicParameters();
+
+                foreach (var item in parameters)
+                {
+                    dynamicParamters.Add(item.ParameterName, item.Value);
+                }
+
+                try
+                {
+                    var data = await conn.ExecuteAsync(sql, dynamicParamters, commandType: CommandType.Text);
+                    return data;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(String.Format("{0}.WithConnection() experienced an exception", GetType().FullName), ex);
+                }
+            });
+        }
+
+        public async Task<T> ExecuteStoredProcedureQueryAsync(string storedProcedure, params SqlParameter[] parameters)
+        {
+            return await WithConnection<T>(async c => {
+
+                List<T> list = new List<T>();
+
+                DynamicParameters dynamicParamters = new DynamicParameters();
+
+                foreach (var item in parameters)
+                {
+                    dynamicParamters.Add(item.ParameterName, item.Value);
+                }
+
+                try
+                {
+                    var data = await conn.QueryAsync<T>(storedProcedure, dynamicParamters, commandType: CommandType.StoredProcedure);
+                    return data.FirstOrDefault();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(String.Format("{0}.WithConnection() experienced an exception", GetType().FullName), ex);
+                }
+            });
+        }
+
+        public async Task<List<IEnumerable<T>>> ExecuteStoredProcedureQueryMultipleAsync(string storedProcedure, params SqlParameter[] parameters)
+        {
+            return await WithConnection<List<IEnumerable<T>>>(async c => {
+
+                List<IEnumerable<IEnumerable<T>>> list = new List<IEnumerable<IEnumerable<T>>>();
+
+                DynamicParameters TParamters = new DynamicParameters();
+
+                foreach (var item in parameters)
+                {
+                    TParamters.Add(item.ParameterName, item.Value);
+                }
+
+                try
+                {
+                    var data = await conn.QueryMultipleAsync(storedProcedure, TParamters, commandType: CommandType.StoredProcedure);
+
+                    List<IEnumerable<T>> d = new List<IEnumerable<T>>();
+                    while (data.IsConsumed == false)
+                    {
+                        d.Add(await data.ReadAsync<T>());
+                    }
+
+                    return d;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(String.Format("{0}.WithConnection() experienced an exception", GetType().FullName), ex);
+                }
+            });
+        }
+    }
+}
